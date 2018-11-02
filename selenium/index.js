fc2279627c4abe0f3735ef3695c9b315d1f4bee7
hirokisan <expsharetoworld@gmail.com>
2018-11-02 21:36:42 +0900

    Use firefox driver to login/logout with dotenv

--- selenium/index.js
+++ selenium/index.js
@@ -1,16 +1,28 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
+require('dotenv').config()
+
+const URL = 'https://guchiatsume.com/';
+const EMAIL = process.env.GUCHI_EMAIL;
+const PASSWORD = process.env.GUCHI_PASSWORD;
+
+
 (async () => {
   //let driver = await new Builder().forBrowser('chrome').build();
-  //let driver = await new Builder().forBrowser('firefox').build();
-  let driver = await new Builder().forBrowser('safari').build();
+  let driver = await new Builder().forBrowser('firefox').build();
+  //let driver = await new Builder().forBrowser('safari').build();
 
   try
   {
-    await driver.get('http://www.google.com/');
+    await driver.get(URL);
+    await driver.findElement(By.linkText('ログイン')).click();
+    await driver.findElement(By.name('email')).sendKeys(EMAIL);
+    await driver.findElement(By.name('password')).sendKeys(PASSWORD, Key.RETURN);
+    let logout = await driver.wait(until.elementLocated(By.linkText('ログアウト')), 10000);
+    await logout.click();
   }
   finally
   {
-    await driver.quit();
+    //await driver.quit();
   }
 })();
