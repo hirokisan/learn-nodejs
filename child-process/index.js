3548785098e0f966cf979fddb591d68b7a0ba497
hirokisan <expsharetoworld@gmail.com>
2018-11-02 18:14:03 +0900

    Use exec

--- child-process/index.js
+++ child-process/index.js
@@ -1,13 +1,22 @@
 const spawn = require('child-process-promise').spawn;
+const exec = require('child-process-promise').exec;
+
+//(async () => {
+//  console.log('start');
+//  await spawn('node', ['./exec.js'], { capture: [ 'stdout', 'stderr' ]})
+//    .then(function (result) {
+//      console.log('[spawn] stdout: ', result.stdout.toString());
+//    })
+//    .catch(function (err) {
+//      console.error('[spawn] stderr: ', err.stderr);
+//    });
+//  console.log('end');
+//})();
 
 (async () => {
-  console.log('start');
-  await spawn('node', ['./exec.js'], { capture: [ 'stdout', 'stderr' ]})
-    .then(function (result) {
-      console.log('[spawn] stdout: ', result.stdout.toString());
-    })
-    .catch(function (err) {
-      console.error('[spawn] stderr: ', err.stderr);
-    });
-  console.log('end');
+  await exec('java -version')
+  .then(result => {
+    console.log('stdout', result.stdout);
+    console.log('stderr', result.stderr);
+  });
 })();
