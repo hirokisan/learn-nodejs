5941616c6eee8186aa93d275c8179c3d752a96fd
hirokisan <expsharetoworld@gmail.com>
2018-11-07 16:07:25 +0900

    Measure executed time

--- handleError/index.js
+++ handleError/index.js
@@ -1,12 +1,19 @@
 const command = require('./command');
+const { performance } = require('perf_hooks');
 
 (async () => {
   try {
+    performance.mark('start');
     await command.run();
     await command.run2();
     await command.run3();
   } catch (err) {
     console.log('catch', err);
+  } finally {
+    performance.mark('end');
+    performance.measure('time', 'start', 'end');
+    let measure = performance.getEntriesByName('time');
+    console.log("npm start:", measure[0].duration, "ms");
   }
 })();
 
