397908e9977d74f8720888d70e79a3942cc05039
hirokisan <expsharetoworld@gmail.com>
2018-11-12 18:28:09 +0900

    Use dateformat

--- overall/index.js
+++ overall/index.js
@@ -1,3 +1,9 @@
 const os = require('os');
+const dateformat = require('dateformat');
 
 console.log(os.homedir());
+
+let now = new Date();
+let date = dateformat(now, 'yyyy-mm-dd-HH-MM');
+
+console.log(date);
