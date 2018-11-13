29fd57bdc7eb5648bc8bc876307856947c746151
hirokisan <expsharetoworld@gmail.com>
2018-11-13 21:18:55 +0900

    Add csv

---
+++ csv/index.js
@@ -0,0 +1,15 @@
+const stringify = require('csv-stringify')
+const fs = require('fs')
+const iconv = require('iconv-lite')
+
+let input = [];
+let header = [];
+
+header.push('id');
+header.push('name');
+input.push(header);
+input.push(['1', 'james']);
+
+stringify(input, (err, output) => {
+  fs.writeFileSync('./file/sample.csv', iconv.encode(output, 'utf-8'))
+});
